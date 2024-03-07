1e47df310444dc6d55972b8bbe95410c77d2e604
aozturk1 <aozturk1@asu.edu>
2024-03-07 14:56:45 -0700

    project setup files and the project model implementation

---
+++ bugTracker/bugTracker/Controllers/ProjectController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using bugTracker.Models;
+
+namespace bugTracker.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            var project = new Project() { Name = "Demo" };
+            return View(project);
+        }
+    }
+}
